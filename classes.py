52dd316a5c59abac34223e42b72d6df95b1cc36e
markd <mark.permyakov.2002@bk.ru>
2020-10-22 22:24:59 +0300

    Конструктор написан, комментарии добавлены, документация сделана.

--- classes.py
+++ classes.py
@@ -1,57 +1,82 @@
 class TicTacToe:
 
-    def draw_field(self, field):
+    def __init__(self, player1, player2, field):
+        self.player1 = player1
+        self.player2 = player2
+        self.field = field
+        # получаем на вход имена обоих игроков и поле, на котором будем работать
+
+    def who_is_who(self):
+        print(self.player1, "is first and playing X")
+        print(self.player2, "is second and playing O")
+
+    def draw_field(self):
         for i in range(3):
             for j in range(3):
-                print(field[i][j], sep="", end="")
+                print(self.field[i][j], sep="", end="")
             print("")
 
-    def game_over(self, turn, field):
-        if turn < 10 and TicTacToe.winner(self, field) == "Draw":
+    def game_over(self, turn):
+        if turn < 10 and TicTacToe.winner(self) == "Draw":
+            # если после очередного хода всё ещё ничья,
+            # и при этом прошло меньше 10 ходов, то есть остались свободные клетки
             return False
         else:
             return True
+            # иначе игра закончилась, то есть наступил game over, возвращаем истину
 
     def whose_turn(self, turn):
         if turn % 2 == 1:
-            print("Now is Player 1 Turn")
+            print("Now is", self.player1, "Turn")
         else:
-            print("Now is Player 2 Turn")
+            print("Now is", self.player2, "Turn")
+            # на нечетных ходах ходит первый, на четных второй
 
-    def nought_turn(self, x, y, field):
-        field[x-1][y-1] = '[O]'
+    def nought_turn(self, x, y):
+        if self.field[x-1][y-1] == '[ ]':
+            self.field[x-1][y-1] = '[O]'
+        else:
+            return -1
 
-    def cross_turn(self, x, y, field):
-        field[x-1][y-1] = '[X]'
+    def cross_turn(self, x, y):
+        if self.field[x - 1][y - 1] == '[ ]':
+            self.field[x-1][y-1] = '[X]'
+        else:
+            return -1
 
-    def winner(self, field):
+    def winner(self):
         s = ''
-        for i in range(3):
-            for j in range(3):
-                s += field[i][j]
+        # посмотрим на все строки, строчки и на обе диагонали
+        # если там есть три одинаковых символа, то кто-то уже выиграл
+        for i in range(0, 3):
+            for j in range(0, 3):
+                s += self.field[i][j]
             if s == '[O][O][O]':
-                return "Player 2 wins"
+                return self.player2 + " wins"
             else:
                 if s == '[X][X][X]':
-                    return "Player 1 wins"
+                    return self.player1 + " wins"
             s = ''
         if s == '':
-            for i in range(3):
-                for j in range(3):
-                    s += field[j][i]
+            # если среди строчек не оказалось, ищем среди столбцов
+            for i in range(0, 3):
+                for j in range(0, 3):
+                    s += self.field[j][i]
                 if s == '[O][O][O]':
-                    return "Player 2 wins"
+                    return self.player2 + " wins"
                 else:
                     if s == '[X][X][X]':
-                        return "Player 1 wins"
-            s = ''
+                        return self.player1 + " wins"
+                s = ''
         if s == '':
-            d1 = field[0][0] + field[1][1] + field[2][2]
-            d2 = field[0][0] + field[1][1] + field[2][2]
+            # если и среди столбцов не оказалось, ищем среди диагоналей
+            d1 = self.field[0][0] + self.field[1][1] + self.field[2][2]
+            d2 = self.field[2][0] + self.field[1][1] + self.field[0][2]
             if d1 == '[O][O][O]' or d2 == '[O][O][O]':
-                return "Player 2 wins"
+                return self.player2 + " wins"
             else:
                 if d1 == '[X][X][X]' or d2 == '[X][X][X]':
-                    return "Player 1 wins"
+                    return self.player1 + " wins"
                 else:
                     return "Draw"
+                    # если нигде не оказалось, то пока ничья
--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 from classes import TicTacToe
 
+name1, name2 = map(str, input().split())
 
 field = [['[ ]', '[ ]', '[ ]'], ['[ ]', '[ ]', '[ ]'], ['[ ]', '[ ]', '[ ]']]
+# начальное поле
+
+game = TicTacToe(name1, name2, field)
+
+game.who_is_who()
+# кто за кого играет
+
 turn = 1
-game = TicTacToe()
-
-
-while not game.game_over(turn, field):
+while not game.game_over(turn):
     game.whose_turn(turn)
+    # кто ходит
     turn += 1
-    game.draw_field(field)
+    game.draw_field()
     x, y = map(int, input().split())
-    if turn % 2 == 0:
-        game.cross_turn(x, y, field)
+    # вводим номер строки (нумерация сверху вниз) и номер столбца (нумерация слева направо)
+    symbol = str(input())
+    # вводим то, что хотим вставить в клетку
+    if symbol == 'X':
+        if game.cross_turn(x, y) != -1:
+            game.cross_turn(x, y)
+        else:
+            print("Turn is invalid: occupied cell")
+            # если ввели допустимый символ, но клетка уже занята
+            turn -= 1
+            # отнимаем один ход, так как его, по сути, и не было
     else:
-        game.nought_turn(x, y, field)
-    game.winner(field)
+        if symbol == 'O':
+            if game.nought_turn(x, y) != -1:
+                game.nought_turn(x, y)
+            else:
+                print("Turn is invalid: occupied cell")
+                turn -= 1
+        else:
+            print("Turn is invalid: invalid character")
+            # если вввели недопустимый символ
+            turn -= 1
+    game.winner()
+    # проверяем, не выйграл ли уже кто-нибудь
 
-game.draw_field(field)
-print(game.winner(field))
+game.draw_field()
+print(game.winner())
