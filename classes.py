f4df01672227205d547745f478551855ee8f6f64
markd <mark.permyakov.2002@bk.ru>
2020-10-22 19:48:35 +0300

    Все методы написаны, остался конструктор

--- classes.py
+++ classes.py
@@ -1,8 +1,57 @@
 class TicTacToe:
 
-    def whose_turn(self):
-        k = 0
-        if k == 0:
-            print("Player 1 Turn")
+    def draw_field(self, field):
+        for i in range(3):
+            for j in range(3):
+                print(field[i][j], sep="", end="")
+            print("")
+
+    def game_over(self, turn, field):
+        if turn < 10 and TicTacToe.winner(self, field) == "Draw":
+            return False
         else:
-            print("Player 2 Turn")
+            return True
+
+    def whose_turn(self, turn):
+        if turn % 2 == 1:
+            print("Now is Player 1 Turn")
+        else:
+            print("Now is Player 2 Turn")
+
+    def nought_turn(self, x, y, field):
+        field[x-1][y-1] = '[O]'
+
+    def cross_turn(self, x, y, field):
+        field[x-1][y-1] = '[X]'
+
+    def winner(self, field):
+        s = ''
+        for i in range(3):
+            for j in range(3):
+                s += field[i][j]
+            if s == '[O][O][O]':
+                return "Player 2 wins"
+            else:
+                if s == '[X][X][X]':
+                    return "Player 1 wins"
+            s = ''
+        if s == '':
+            for i in range(3):
+                for j in range(3):
+                    s += field[j][i]
+                if s == '[O][O][O]':
+                    return "Player 2 wins"
+                else:
+                    if s == '[X][X][X]':
+                        return "Player 1 wins"
+            s = ''
+        if s == '':
+            d1 = field[0][0] + field[1][1] + field[2][2]
+            d2 = field[0][0] + field[1][1] + field[2][2]
+            if d1 == '[O][O][O]' or d2 == '[O][O][O]':
+                return "Player 2 wins"
+            else:
+                if d1 == '[X][X][X]' or d2 == '[X][X][X]':
+                    return "Player 1 wins"
+                else:
+                    return "Draw"
--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 from classes import TicTacToe
 
+
+field = [['[ ]', '[ ]', '[ ]'], ['[ ]', '[ ]', '[ ]'], ['[ ]', '[ ]', '[ ]']]
+turn = 1
 game = TicTacToe()
-game.whose_turn()
+
+
+while not game.game_over(turn, field):
+    game.whose_turn(turn)
+    turn += 1
+    game.draw_field(field)
+    x, y = map(int, input().split())
+    if turn % 2 == 0:
+        game.cross_turn(x, y, field)
+    else:
+        game.nought_turn(x, y, field)
+    game.winner(field)
+
+game.draw_field(field)
+print(game.winner(field))
